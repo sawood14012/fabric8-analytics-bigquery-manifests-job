f77b5f7f36e3fce40dd49d86fb0fcdefa8a9cfe8
dipanjanS <dsarkar@redhat.com>
2019-05-14 10:13:51 +0530

    updated s3 bucket name

--- src/config/cloud_constants.py
+++ src/config/cloud_constants.py
@@ -11,5 +11,5 @@ USE_CLOUD_SERVICES = True
 AWS_S3_REGION = 'us-east-1'
 AWS_S3_ACCESS_KEY_ID = os.environ.get('AWS_S3_ACCESS_KEY_ID', '')
 AWS_S3_SECRET_ACCESS_KEY = os.environ.get('AWS_S3_SECRET_ACCESS_KEY', '')
-S3_BUCKET_NAME = 'dsarkar-developer-analytics-audit-report'
+S3_BUCKET_NAME = 'developer-analytics-audit-report'
 MANIFEST_FILENAME = 'collated.json'
