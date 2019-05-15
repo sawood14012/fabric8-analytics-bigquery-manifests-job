98f4143b8ce32e427d51ccf68b12c65af7c57009
dipanjanS <dsarkar@redhat.com>
2019-05-15 12:32:54 +0530

    updated dockerfiles

--- create_localdev_env.sh
+++ create_localdev_env.sh
@@ -2,4 +2,4 @@ virtualenv venv
 
 source ./venv/bin/activate
 pip3 install --upgrade pip
-pip3 install --ignore-installed git+https://github.com/fabric8-analytics/fabric8-analytics-rudra.git@e59b708b22e094f356c218debdfa287f142337d8#egg=rudra
+pip3 install --ignore-installed git+https://github.com/fabric8-analytics/fabric8-analytics-rudra.git@4cfac114208cd0f062a1d7ab0e03bb6bc7a490a3#egg=rudra
