eabf5a83439e2c4abff93bcb571c84950a6e25b6
dipanjanS <dsarkar@redhat.com>
2019-05-15 12:33:39 +0530

    Added cico scripts

---
+++ cico_setup.sh
@@ -0,0 +1,77 @@
+#!/bin/bash -ex
+
+REGISTRY=$(make get-push-registry)
+
+load_jenkins_vars() {
+    if [ -e "jenkins-env.json" ]; then
+        eval "$(./env-toolkit load -f jenkins-env.json \
+                DEVSHIFT_TAG_LEN \
+                QUAY_USERNAME \
+                QUAY_PASSWORD \
+                JENKINS_URL \
+                GIT_BRANCH \
+                GIT_COMMIT \
+                BUILD_NUMBER \
+                ghprbSourceBranch \
+                ghprbActualCommit \
+                BUILD_URL \
+                ghprbPullId)"
+    fi
+}
+
+docker_login() {
+    if [ -n "${QUAY_USERNAME}" -a -n "${QUAY_PASSWORD}" ]; then
+        docker login -u "${QUAY_USERNAME}" -p "${QUAY_PASSWORD}" "${REGISTRY}"
+    else
+        echo "Could not login, missing credentials for the registry"
+        exit 1
+    fi
+}
+
+prep() {
+    yum -y update
+    yum -y install epel-release
+    yum -y install git which python36-pip python36-virtualenv gcc openssl-devel docker
+    systemctl start docker
+}
+
+build_image() {
+    # login before build to be able to pull RHEL parent image
+    docker_login
+    make docker-build
+}
+
+tag_push() {
+    local target=$1
+    local source=$2
+    docker tag "${source}" "${target}"
+    docker push "${target}"
+}
+
+push_image() {
+    local image_name
+    local image_repository
+    local short_commit
+    image_name=$(make get-image-name)
+    image_repository=$(make get-image-repository)
+    short_commit=$(git rev-parse --short=7 HEAD)
+    push_registry=$(make get-push-registry)
+
+    docker_login
+
+    if [ -n "${ghprbPullId}" ]; then
+        # PR build
+        pr_id="SNAPSHOT-PR-${ghprbPullId}"
+        tag_push "${push_registry}/${image_repository}:${pr_id}" "${image_name}"
+        tag_push "${push_registry}/${image_repository}:${pr_id}-${short_commit}" "${image_name}"
+    else
+        # master branch build
+        tag_push "${push_registry}/${image_repository}:latest" "${image_name}"
+        tag_push "${push_registry}/${image_repository}:${short_commit}" "${image_name}"
+    fi
+
+    echo 'CICO: Image pushed, ready to update deployed app'
+}
+
+load_jenkins_vars
+prep
